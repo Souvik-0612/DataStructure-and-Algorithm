0dc85026cab5d6b2425916cb86ffe902e8721a1d
Souvik-0612 <bridgesouvik@gmail.com>
2023-06-04 11:20:50 +0530

    singly linked list

---
+++ 3. Lineked Lists/1. Singly_linked_list.py
@@ -0,0 +1,18 @@
+class Node:
+    #Constructor
+    def __init__(self, data = None, next = None):
+        self.data = data
+        self.next = next
+    #method for setting the data field of the node
+    def setData(self, data):
+        self.data = data
+    #method for getting the data field of the node
+    def getData(self):
+        return self.data
+    #method for setting the next field of the node
+    def setNext(self, data):
+        self.next = next
+    #method for getting the next field of the node
+    def getNext(self, data):
+        self.next
+
